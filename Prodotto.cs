5e1c00fc6f4670bc77b5b69826e20fd12c6156f7
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-06-29 14:32:59 +0200

    code product random

---
+++ Prodotto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        public int CodeProduct
+        {
+            get
+            {
+                int codeRdn = new Random().Next(0, 999999999);
+                return codeRdn;
+            }
+        }
+        public int NameProduct { get; set; }
+        public int DescriptionProduct { get; set; }
+        public int PriceProduct { get; set; }
+        public int IvaProduct { get; set; }
+
+    }
+}
